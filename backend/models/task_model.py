2b78cfaff4d15f65699c28e35e58714624781cd1
Pedro Ruan <pedroruan17@hotmail.com>
2025-04-07 22:47:55 -0300

    Add files via upload

--- backend/models/task_model.py
+++ backend/models/task_model.py
@@ -3,10 +3,11 @@ from datetime import datetime
 from typing import Optional
 
 class Task(BaseModel):
-	title: str = Field(..., description="Título da tarefa")
-	description: Optional[str] = Field(None, description="Descrição da terefa")
-	due_date: Optional[datetime] = Field(None, description="Data e hora de conclusão")
-	priority: Optional[str] = Field("normal", description="Prioridade da tarefa: baixa, normal, alta")
-	completed: bool = False
-	category: str = Field(..., description="Tipo: tarefa, evento, lembrete, nota")
-	created_at: datetime = Field(default_factory=datetime.utcnow)
+    title: str = Field(..., description="Título da tarefa")
+    description: Optional[str] = Field(None, description="Descrição da tarefa")
+    due_date: Optional[datetime] = Field(None, description="Data e hora de conclusão")
+    priority: Optional[str] = Field("normal", description="Prioridade da tarefa: baixa, normal, alta")
+    completed: bool = False
+    category: str = Field(..., description="Tipo: tarefa, evento, lembrete, nota")
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    user_id: Optional[str] = Field(None, description="ID do usuário dono da tarefa")  # 
