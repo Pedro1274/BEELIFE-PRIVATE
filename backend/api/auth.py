ffcdb05d6ba6867ed148982935d65a0f2ce2e244
Pedro Ruan <pedroruan17@hotmail.com>
2025-04-09 13:41:05 -0300

    Add files via upload

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,27 +1,32 @@
-from fastapi import Request, HTTPException
+from fastapi import Request
+from starlette.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 from database.token import verify_access_token
 from database.database import users_collection
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        if (request.url.path.startswith("/api/tasks") or request.url.path.startswith("/api/patterns")) \
-            and not request.url.path.endswith("/google-event"):
-            auth_header = request.headers.get("Authorization")
-            if not auth_header or not auth_header.startswith("Bearer "):
-                raise HTTPException(status_code=401, detail="Token não encontrado")
+        try:
+            if (request.url.path.startswith("/api/tasks") or request.url.path.startswith("/api/patterns")) \
+                and not request.url.path.endswith("/google-event"):
+                auth_header = request.headers.get("Authorization")
+                if not auth_header or not auth_header.startswith("Bearer "):
+                    return JSONResponse(status_code=401, content={"detail": "Token não encontrado"})
 
-            token = auth_header.split(" ")[1]
-            payload = verify_access_token(token)
-            if not payload:
-                raise HTTPException(status_code=401, detail="Token inválido")
+                token = auth_header.split(" ")[1]
+                payload = verify_access_token(token)
+                if not payload:
+                    return JSONResponse(status_code=401, content={"detail": "Token inválido"})
 
-            username = payload.get("sub")
-            user = await users_collection.find_one({"username": username})
-            if not user:
-                raise HTTPException(status_code=401, detail="Usuário não encontrado")
+                username = payload.get("sub")
+                user = await users_collection.find_one({"username": username})
+                if not user:
+                    return JSONResponse(status_code=401, content={"detail": "Usuário não encontrado"})
 
-            request.state.user = user
+                request.state.user = user
 
-        response = await call_next(request)
-        return response
+            response = await call_next(request)
+            return response
+
+        except Exception as e:
+            return JSONResponse(status_code=500, content={"detail": str(e)})
